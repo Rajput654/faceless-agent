c2253f258658ff12f0f5a9ccd57f4e238fa4770b
Sanskar Rajput <160146829+Rajput654@users.noreply.github.com>
2026-03-28 20:37:48 +0530

    Add VideoFetcherMCPServer to imports and exports

--- mcp_servers/__init__.py
+++ mcp_servers/__init__.py
@@ -1,9 +1,10 @@
-from mcp_servers.scraper_server import ScraperMCPServer
-from mcp_servers.tts_server import TTSMCPServer
-from mcp_servers.image_server import ImageMCPServer
-from mcp_servers.video_server import VideoMCPServer
-from mcp_servers.music_server import MusicMCPServer
-from mcp_servers.social_server import SocialMCPServer
+from mcp_servers.scraper_server  import ScraperMCPServer
+from mcp_servers.tts_server      import TTSMCPServer
+from mcp_servers.image_server    import ImageMCPServer
+from mcp_servers.video_server    import VideoMCPServer
+from mcp_servers.video_fetcher   import VideoFetcherMCPServer
+from mcp_servers.music_server    import MusicMCPServer
+from mcp_servers.social_server   import SocialMCPServer
 from mcp_servers.analytics_server import AnalyticsMCPServer
 
 __all__ = [
@@ -11,6 +12,7 @@ __all__ = [
     "TTSMCPServer",
     "ImageMCPServer",
     "VideoMCPServer",
+    "VideoFetcherMCPServer",
     "MusicMCPServer",
     "SocialMCPServer",
     "AnalyticsMCPServer",
